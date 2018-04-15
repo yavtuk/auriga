013d40c034dbeae3d1a43e1842bf5dd6a8987845
Yavtuk <man@big-3.ru>
2018-04-15 17:30:50 +0300

    Change a calculation of delta time, change container of result data

--- python/hw_1_3/log_parser.py
+++ python/hw_1_3/log_parser.py
@@ -1,54 +1,93 @@
 #!/usr/bin/env python3
 import re
+from datetime import datetime, timedelta
 
 
 def log_parser(file_name):
-    start_dict = dict()
-    finish_dict = dict()
-    error_dict = dict()
+    stats = dict()
 
+    l = list()
     with open(file_name, "r") as fin:
+        l.clear()
         for line in fin:
             data = re.sub('[<>\[\]]', '', line).split()
-            if data[2] == "INFO":
-                if data[3] == "Started":
-                    start_dict[int(data[-1])] = data[1]
-                elif data[3] == "Finished":
-                    finish_dict[int(data[-1])] = data[1]
-            else:
-                error_dict[int(data[-1])] = data[-1]
+            packed_list = prepare_list(data)
+            add_data_to_dict(packed_list, stats)
     fin.close()
+    exec_time_print(stats)
+    err_msg_print(stats)
 
-    exec_time_print(start_dict, finish_dict)
-    err_msg_print(error_dict)
+def add_data_to_dict(data, res):
+    indx = int(data.pop(0))
 
+    try:
+        d = res[indx]
+    except:
+        d = {'sDate': None, 'eDate': None, 'sTime': None, 'eTime': None, 'res_msg': None, 'err_msg': None}
 
-def exec_time_print(stime, etime):
-    for x in stime:
-        try:
-            s = stime[int(x)]
-            e = etime[int(x)]
-            print("Request {0}: {1: 8.6f}".format(x, get_period_in_sec(s, e)))
-        except:
-            pass
+    msg = data.pop()
+    if data.pop(0) == 'INFO':
+        if 0 == msg.find('Started'):
+            d['sDate'] = data.pop(0)
+            d['sTime'] = data.pop(0)
+        elif 0 == msg.find('Finished'):
+            d['eDate'] = data.pop(0)
+            d['eTime'] = data.pop(0)
+    else:
+        d['err_msg'] = msg
 
-def err_msg_print(errors_dict):
+    res[indx] = d
+
+def prepare_list(data):
+    """
+    [0] = id req
+    [1] = date
+    [2] = time
+    [3] = type msg
+    [4] =
+    [4] = err msg
+    :return: packed list
+    """
+    pack_list = list()
+    pack_list.append(data[-1])
+    pack_list.append(data[2])
+    pack_list.append(data[0])
+    pack_list.append(data[1])
+
+    msg = ""
+    for x in range(3, len(data) - 3):
+        msg += data[x]
+        msg += ' '
+
+    pack_list.append(msg)
+    return pack_list
+
+def exec_time_print(data):
+    for x in data:
+        info = data[x]
+        if info['eDate'] and info['sDate']:
+            syear, smonth, sday = re.sub('[-]', ' ', info['sDate']).split()
+            shour, sminute, ssecond = re.sub('[:]', ' ', info['sTime']).split()
+            ssecond, smicrosec = re.sub('[.]', ' ', ssecond).split()
+            s = datetime(year=int(syear), month=int(smonth), day=int(sday), hour=int(shour),
+                          minute=int(sminute), second=int(ssecond), microsecond=int(smicrosec))
+
+            eyear, emonth, eday = re.sub('[-]', ' ', info['eDate']).split()
+            ehour, eminute, esecond = re.sub('[:]', ' ', info['eTime']).split()
+            esecond, emicrosec = re.sub('[.]', ' ', esecond).split()
+            e = datetime(year=int(eyear), month=int(emonth), day=int(eday), hour=int(ehour),
+                          minute=int(eminute), second=int(esecond), microsecond=int(emicrosec))
+            print("Request %d: %s"%(x, (e-s).total_seconds()))
+        else:
+            print("Request %d: -1"%x)
+
+def err_msg_print(data):
+    print("")
     print("Errors")
-    for x in errors_dict:
-        print("Request %d: Calculation failed" % x)
-
-def get_period_in_sec(stm, etm):
-    precision = 1000000
-    hs, ms, ss = re.sub('[:]', ' ', stm).split()
-    ss, mss = re.sub('[.]', ' ', ss).split()
-    he, me, se = re.sub('[:]', ' ', etm).split()
-    se, mse = re.sub('[.]', ' ', se).split()
-    mss = (((int(hs) * 3600) + (int(ms) * 60) + (int(ss))) * precision) + int(mss)
-    mse = (((int(he) * 3600) + (int(me) * 60) + (int(se))) * precision) + int(mse)
-    delta = mse - mss
-    sec = delta / precision
-    ms = (delta % precision) / precision
-    return sec + ms
+    for x in data:
+        info = data[x]
+        if info['err_msg'] is not None:
+            print("Request %d: %s"%(x, info['err_msg']))
 
 if __name__ == "__main__":
     log_parser("logs/task_1_2.log")
